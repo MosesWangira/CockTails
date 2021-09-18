c0fe832ffed7af01142d7938e0024f481702c46e
altruism <proudmoses@gmail.com>
2021-09-18 16:49:55 +0300

    change launcher activity

---
+++ app/src/main/java/com/mosesaltruism/cocktails/CockTails.kt
@@ -0,0 +1,9 @@
+package com.mosesaltruism.cocktails
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CockTails : Application() {
+
+}
--- app/src/main/java/com/mosesaltruism/cocktails/core/common/base/DashBoard.kt
+++ app/src/main/java/com/mosesaltruism/cocktails/core/common/base/DashBoard.kt
@@ -1,12 +1,12 @@
-package com.mosesaltruism.cocktails
+package com.mosesaltruism.cocktails.core.common.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mosesaltruism.cocktails.R
 
-class MainActivity : AppCompatActivity() {
+class DashBoard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.dash_board)
     }
-
 }
