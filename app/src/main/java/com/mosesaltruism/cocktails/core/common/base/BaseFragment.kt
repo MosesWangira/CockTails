a355ed8bc60c86bb24c4cce2167beda86017494d
altruism <proudmoses@gmail.com>
2021-09-19 10:47:16 +0300

    add base fragment

---
+++ app/src/main/java/com/mosesaltruism/cocktails/core/common/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.mosesaltruism.cocktails.core.common.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding> : Fragment() {
+    protected lateinit var binding: T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            getFragmentView(),
+            container,
+            false
+        )
+
+        return binding.root
+    }
+
+    //gives xml layout id to show in fragment
+    //returns layout id
+    abstract fun getFragmentView(): Int
+}
--- app/src/main/java/com/mosesaltruism/cocktails/presentation/byname/Home.kt
+++ app/src/main/java/com/mosesaltruism/cocktails/presentation/byname/Home.kt
@@ -1,18 +1,10 @@
 package com.mosesaltruism.cocktails.presentation.byname
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.mosesaltruism.cocktails.R
+import com.mosesaltruism.cocktails.core.common.base.BaseFragment
+import com.mosesaltruism.cocktails.databinding.HomeBinding
 
-class Home : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home, container, false)
-    }
+class Home : BaseFragment<HomeBinding>() {
+    override fun getFragmentView(): Int = R.layout.home
+
 }
