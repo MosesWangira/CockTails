43b8b2f17338086435407e271bfaf311d86321ff
altruism <proudmoses@gmail.com>
2021-09-22 06:44:37 +0300

    add datastore class implementation

---
+++ app/src/main/java/com/mosesaltruism/cocktails/core/common/base/DataStorePreference.kt
@@ -0,0 +1,37 @@
+package com.mosesaltruism.cocktails.core.common.base
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import com.mosesaltruism.cocktails.core.common.util.Constants.COCKTAIL_NAME
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "data_store")
+
+class DataStorePreference @Inject constructor(@ApplicationContext context: Context) {
+
+    private val appContext = context.applicationContext
+
+    val searchedCocktailName: Flow<String?>
+        get() = appContext.dataStore.data.map { preferences ->
+            preferences[COCKTAIL_NAME]
+        }
+
+
+    suspend fun saveAccessTokens(accessToken: String, refreshToken: String) {
+        appContext.dataStore.edit { preferences ->
+            preferences[COCKTAIL_NAME] = accessToken
+        }
+    }
+
+    suspend fun clear() {
+        appContext.dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+}
--- app/src/main/java/com/mosesaltruism/cocktails/core/common/util/Constants.kt
+++ app/src/main/java/com/mosesaltruism/cocktails/core/common/util/Constants.kt
@@ -1,6 +1,10 @@
 package com.mosesaltruism.cocktails.core.common.util
 
+import androidx.datastore.preferences.core.stringPreferencesKey
+
 object Constants {
     const val BASE_URL = "https://thecocktaildb.com/api/json/v1/1/"
     const val DATABASE_NAME = "cocktail_db"
+
+    val COCKTAIL_NAME = stringPreferencesKey("searched_cocktail_name")
 }
