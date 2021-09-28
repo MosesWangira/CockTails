17a62c2f567fce2608aeace577a62b33d6aff311
altruism <proudmoses@gmail.com>
2021-09-28 11:35:38 +0300

    remove non null in entity

--- app/src/main/java/com/mosesaltruism/cocktails/domain/byname/entities/search/SearchedCockTailItem.kt
+++ app/src/main/java/com/mosesaltruism/cocktails/domain/byname/entities/search/SearchedCockTailItem.kt
@@ -1,6 +1,5 @@
 package com.mosesaltruism.cocktails.domain.byname.entities.search
 
-import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -13,47 +12,36 @@ data class SearchedCockTailItem constructor(
     @PrimaryKey
     val idDrink: String,
 
-    @NonNull
     @ColumnInfo(name = "dateModified")
     val dateModified: String,
 
-    @NonNull
     @ColumnInfo(name = "strAlcoholic")
     val strAlcoholic: String,
 
-    @NonNull
     @ColumnInfo(name = "strCategory")
     val strCategory: String,
 
-    @NonNull
     @ColumnInfo(name = "strCreativeCommonsConfirmed")
     val strCreativeCommonsConfirmed: String,
 
-    @NonNull
     @ColumnInfo(name = "strDrink")
     val strDrink: String,
 
-    @NonNull
     @ColumnInfo(name = "strDrinkThumb")
     val strDrinkThumb: String,
 
-    @NonNull
     @ColumnInfo(name = "strGlass")
     val strGlass: String,
 
-    @NonNull
     @ColumnInfo(name = "strImageSource")
     val strImageSource: String,
 
-    @NonNull
     @ColumnInfo(name = "strInstructions")
     val strInstructions: String,
 
-    @NonNull
     @ColumnInfo(name = "strInstructionsDE")
     val strInstructionsDE: String,
 
-    @NonNull
     @ColumnInfo(name = "strInstructionsIT")
     val strInstructionsIT: String
 )
